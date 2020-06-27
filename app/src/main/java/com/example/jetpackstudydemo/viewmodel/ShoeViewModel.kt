a71ba4021d01d7721b9f5c47728f8ee3931d9c9d
Tony.li <tony.li@gaiaworks.cn>
2020-06-27 15:19:58 +0800

    永远只暴露不可变的LiveData给外部

--- app/src/main/java/com/example/jetpackstudydemo/viewmodel/ShoeViewModel.kt
+++ app/src/main/java/com/example/jetpackstudydemo/viewmodel/ShoeViewModel.kt
@@ -1,20 +1,23 @@
 package com.example.jetpackstudydemo.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ShoeViewModel(countReserved :Int):ViewModel() {
-    var counter = MutableLiveData<Int>()
+    val counter : LiveData<Int>
+        get() = _counter
+    private  var _counter = MutableLiveData<Int>()
     init {
-        counter.value = countReserved
+        _counter.value = countReserved
     }
 
     fun plusOne(){
-        val count = counter.value?:0
-        counter.value = count+1
+        val count = _counter.value?:0
+        _counter.value = count+1
     }
 
     fun clear() {
-        counter.value = 0
+        _counter.value = 0
     }
 }
