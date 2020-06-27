765d7debb4c2b6b0033086a438623b25d0140ec7
Tony.li <tony.li@gaiaworks.cn>
2020-06-27 15:11:56 +0800

    LiveData基本用法

--- app/src/main/java/com/example/jetpackstudydemo/ui/fragment/ShoeFragment.kt
+++ app/src/main/java/com/example/jetpackstudydemo/ui/fragment/ShoeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.jetpackstudydemo.R
 import com.example.jetpackstudydemo.ui.activity.MyLifeCycleObserver
@@ -28,20 +29,14 @@ class ShoeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val countReserved : Int = 0
         shoeViewModel = ViewModelProviders.of(this,ShoeViewModelFactroy(countReserved)).get(ShoeViewModel::class.java)
-//        shoeViewModel = ViewModelProviders.of(this).get(ShoeViewModel::class.java)
         plusOneBtn.setOnClickListener {
-            shoeViewModel.counter++
-            refreshCounter()
+            shoeViewModel.plusOne()
         }
         clearBtn.setOnClickListener {
-            shoeViewModel.counter = 0
+            shoeViewModel.clear()
             Toast.makeText(context, "获取当前Fragment生命周期"+lifecycle.currentState.toString(), Toast.LENGTH_SHORT).show()
-            refreshCounter()
         }
-        refreshCounter()
+        shoeViewModel.counter.observe(this, Observer { count -> infoText.text = count.toString() })
     }
 
-    private fun refreshCounter(){
-        infoText.text = shoeViewModel.counter.toString()
-    }
 }
--- app/src/main/java/com/example/jetpackstudydemo/viewmodel/ShoeViewModel.kt
+++ app/src/main/java/com/example/jetpackstudydemo/viewmodel/ShoeViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.jetpackstudydemo.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ShoeViewModel(countReserved :Int):ViewModel() {
-    var counter = countReserved
+    var counter = MutableLiveData<Int>()
+    init {
+        counter.value = countReserved
+    }
+
+    fun plusOne(){
+        val count = counter.value?:0
+        counter.value = count+1
+    }
+
+    fun clear() {
+        counter.value = 0
+    }
 }
